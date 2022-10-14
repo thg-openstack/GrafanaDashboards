311fdceafec0a103dc70ed4af0dbac886b409fc9
Tristan Otterpohl <Tristan.Otterpohl75@thehutgroup.com>
2022-10-14 16:24:08 +0100

    cleaned up

--- GenerateConfigMaps.ps1
+++
@@ -1,4 +0,0 @@
-get-childitem -recurse "*.json" | Foreach-Object { 
-    $output_path = Join-Path $_.DirectoryName "output"
-    Write-Host "kubectl create configmap $($_.BaseName) -n observability --from-file=$($_.FullName) --dry-run=client -oyaml | kubectl label -f- --dry-run=client -o yaml --local grafana_dashboard=""1"" > $output_path/$($_.baseName).yaml"
-}
